cd36e546523c34c2127f47c3743334c558728d54
EugeHysa <euxhenio.hysa01@universitadipavia.it>
2018-02-15 15:23:10 -0700

    Now using online db

--- src/java/DatabaseElements/DBConnection.java
+++ src/java/DatabaseElements/DBConnection.java
@@ -32,10 +32,8 @@ public class DBConnection
             //conn = DriverManager.getConnection("jdbc:sqlite:sqlite.db");
             //togliere commenti per MYSQL
             Class.forName("com.mysql.jdbc.Driver");
-            /*JAMES "root", "12345");*/
-            /*ANTONINO "root", "Prove");*/
-            /*GIOVANNA "root", "Giovanna26");*/
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/Components", "root", "untothedead94");
+            
+            conn = DriverManager.getConnection("jdbc:mysql://den1.mysql2.gear.host:3306/progettof", "progettof", "Antonino-00");
             mystmt = conn.createStatement();
             }
         catch(ClassNotFoundException ex)
